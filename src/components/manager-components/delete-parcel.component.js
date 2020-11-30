30e9efebceee47ff013e840b3e96524d9675b04e
Hydrogen3105 <porinw2000@gmail.com>
2020-11-30 14:02:27 +0700

    update v.0018 - wh validations and delete parcel

--- src/components/manager-components/delete-parcel.component.js
+++ src/components/manager-components/delete-parcel.component.js
@@ -176,6 +176,7 @@ class DeleteParcel extends Component {
                         <button className="btn btn-danger btn-block" 
                                 style={{width: 100}}
                                 onClick={this.handleDelete}
+                                disabled={!(this.state.selectedParcels.length > 0)}
                         >
                             Delete
                         </button>
@@ -196,7 +197,7 @@ class DeleteParcel extends Component {
                 }
                 {
                     this.props.dialog_state === 1 ? 
-                    <QuestionDialog topic='delete-parcel' data={{parcelId: this.state.selectedParcels}}/> :
+                    <QuestionDialog topic='delete-parcel' data={{parcels: this.state.selectedParcels}}/> :
                     this.props.dialog_state === 2 && 
                     <ConfirmedDialog topic='delete-parcel' />
                 }
